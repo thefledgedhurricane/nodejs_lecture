33e3553b0fd632a866b4926c983ba8ec04605ad3
IHABABDELBASSET ANNAKI <iannaki.developer@gmail.com>
2021-04-26 23:50:42 +0000

    [update] installing express and MongoDb dependencies & setting up MongoDB connection

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI:"your connection string it can be found when you connect your cluster and link your app to yor MongoDB Atlas cluster"
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to MongoDB")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
+
